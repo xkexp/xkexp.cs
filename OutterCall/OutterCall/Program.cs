276c5a4062019bbda64c7e1ce57242036efe552b
xkyii <xkyii@foxmail.com>
2022-02-14 11:10:33 +0800

    Add: OutterCall, 调用外部进程

---
+++ OutterCall/OutterCall/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var process = new Process();
+process.StartInfo.FileName = "https";
+process.StartInfo.Arguments = "httpie.io/hello";
+process.Start();
+
+process.WaitForExit();
