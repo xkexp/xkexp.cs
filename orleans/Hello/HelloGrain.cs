ab521d2fb1943828a276a5728f013302270cd811
xkyii <xkyii@foxmail.com>
2022-02-15 16:37:22 +0800

    Add: Orleans.Hello

---
+++ orleans/Hello/HelloGrain.cs
@@ -0,0 +1,10 @@
+﻿namespace xk.experiment.OfOrleans.Hello
+{
+    internal class HelloGrain : Orleans.Grain, IHelloGrain
+    {
+        public Task<string> SayHello(string greeting)
+        {
+            return Task.FromResult($"Hello, {greeting}!");
+        }
+    }
+}
---
+++ orleans/Hello/IHelloGrain.cs
@@ -0,0 +1,7 @@
+﻿namespace xk.experiment.OfOrleans.Hello
+{
+    public interface IHelloGrain : Orleans.IGrainWithStringKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+}
