4cab565bcb5ae6e0d30fbdce50e583f0d1d3dd7f
xkyii <xkyii@foxmail.com>
2022-02-25 14:48:29 +0800

    Add: delegate 注入测试

---
+++ Base/DelegateDi/Decoder.cs
@@ -0,0 +1,31 @@
+﻿namespace DelegateDi;
+
+internal class Decoder
+{
+    public delegate bool HeadChecker(object message);
+
+    private readonly HeadChecker _checker = _ => true;
+
+    public Decoder()
+    {
+        Console.WriteLine("Decoder non args Constructor.");
+    }
+
+    public Decoder(HeadChecker checker)
+    {
+        Console.WriteLine("Decoder checker args Constructor.");
+        _checker = checker;
+    }
+
+    public void Decode(object message)
+    {
+        if (_checker.Invoke(message))
+        {
+            Console.WriteLine("Header Check success");
+        }
+        else
+        {
+            Console.WriteLine("Header Check failed");
+        }
+    }
+}
---
+++ Base/DelegateDi/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using DelegateDi;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello DelegateDi.");
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<Decoder.HeadChecker>(_ => (o) => true);
+        services.AddSingleton<Decoder>();
+        services.AddHostedService<HelloService>();
+    })
+    .Build()
+    .Run();
+
+
+class HelloService : IHostedService
+{
+    private readonly Decoder _decoder;
+    public HelloService(Decoder decoder)
+    {
+        _decoder = decoder;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _decoder.Decode(0);
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
